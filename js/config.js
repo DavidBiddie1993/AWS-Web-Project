2680bbca746465d6ccbf9848f6c533fca8e4aec1
DavidBiddie1993 <davidbiddulph18@gmail.com>
2025-10-27 11:27:24 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_fnQOtJaOS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1k1d51c20it3so76ti0fvakmmf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
